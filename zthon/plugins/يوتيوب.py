3f4346ef1ee5ce52948a4d483412218452e9cf1b
senzir1 <ywsfswyd754@gmail.com>
2025-07-30 21:07:31 +0300

    Update يوتيوب.py

--- zthon/plugins/يوتيوب.py
+++ zthon/plugins/يوتيوب.py
@@ -69,7 +69,7 @@ SONG_SENDING_STRING = "<b>╮ جـارِ تحميـل المقطـٓع الصٓ�
 
 
 def get_cookies_file():
-    folder_path = f"{os.getcwd()}/zxaaxc"
+    folder_path = f"{os.getcwd()}/bin"
     txt_files = glob.glob(os.path.join(folder_path, '*.txt'))
     if not txt_files:
         raise FileNotFoundError("No .txt files found in the specified folder.")
